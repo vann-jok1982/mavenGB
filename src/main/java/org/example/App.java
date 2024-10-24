3e884bafd154fe8b2a4ab4801afbf2f3e79e5ad8
Vann-jok <vann-jok@rambler.ru>
2024-10-25 01:43:22 +0300

    Maven

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.json.JSONString;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Person person=new Person("Иван","Иванов",41);
+        System.out.println(person);
+
+    }
+}
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
